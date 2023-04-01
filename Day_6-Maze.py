2b73bba3a128f9f67d48853377f43767cc3af7eb
Latika M Mohanty <106544035+mohantylatika08@users.noreply.github.com>
2023-04-01 21:29:49 +0530

    Add files via upload

---
+++ Day_6-Maze.py
@@ -0,0 +1,19 @@
+#Copy and paste this link to your web browser to try this code and solve the maze:
+#shorturl.at/fhmMX
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
