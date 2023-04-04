8ab2ea1d8a20ec469d6e80c41f3a97e5e132208f
Latika M Mohanty <106544035+mohantylatika08@users.noreply.github.com>
2023-04-04 07:54:36 +0530

    Add files via upload

---
+++ Day_8- Caesar Cipher/main (2).py
@@ -0,0 +1,37 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+  
+from art import logo
+
+print(logo)
+
+go = True
+
+while go == True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+  
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  
+  go_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if go_again == "yes":
+    go = True 
+  else:
+    go = False
+    print("Goodbye")
